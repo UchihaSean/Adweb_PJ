9a77c51ee0dafb7cc06350cfab6e612216b72895
YiZhou <dugu9sword@163.com>
2016-07-02 16:01:39 +0800

    ok

--- server/db.sql
+++ server/db.sql
@@ -1,49 +1,180 @@
-DROP DATABASE IF EXISTS adweb;
+-- MySQL dump 10.13  Distrib 5.6.30, for debian-linux-gnu (i686)
+--
+-- Host: localhost    Database: adweb
+-- ------------------------------------------------------
+-- Server version	5.6.30-0ubuntu0.14.04.1
 
-CREATE DATABASE adweb CHARACTER SET UTF8;
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
-CREATE TABLE adweb.user(
-  uid INT PRIMARY KEY AUTO_INCREMENT,
-  name VARCHAR(20),
-  password VARCHAR(20),
-  portrait VARCHAR(10)
-)CHARACTER SET UTF8;
+--
+-- Table structure for table `action`
+--
 
-CREATE TABLE adweb.view(
-  vid INT PRIMARY KEY AUTO_INCREMENT,
-  category INT,
-  name VARCHAR(20),
-  longitude FLOAT,
-  latitude FLOAT,
-  detail VARCHAR(100),
-  picture VARCHAR(10)
-)CHARACTER SET UTF8;
+DROP TABLE IF EXISTS `action`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `action` (
+  `uid` int(11) DEFAULT NULL,
+  `vid` int(11) DEFAULT NULL,
+  `aid` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-CREATE TABLE adweb.action(
-  uid INT,
-  vid INT,
-  aid INT
-)CHARACTER SET UTF8;
+--
+-- Dumping data for table `action`
+--
 
-CREATE TABLE adweb.flag(
-  fid INT,
-  vid INT,
-  longitude FLOAT,
-  latitude FLOAT,
-  addtion VARCHAR(100)
-)CHARACTER SET UTF8;
+LOCK TABLES `action` WRITE;
+/*!40000 ALTER TABLE `action` DISABLE KEYS */;
+/*!40000 ALTER TABLE `action` ENABLE KEYS */;
+UNLOCK TABLES;
 
-CREATE TABLE adweb.comment(
-  cid INT PRIMARY KEY AUTO_INCREMENT,
-  vid INT,
-  uid INT,
-  grade INT,
-  detail VARCHAR(100)
-)CHARACTER SET UTF8;
+--
+-- Table structure for table `comment`
+--
 
-CREATE TABLE adweb.resource(
-  cid INT,
-  url VARCHAR(10),
-  type INT,
-  addtion VARCHAR(100)
-)CHARACTER SET UTF8;
+DROP TABLE IF EXISTS `comment`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `comment` (
+  `cid` int(11) NOT NULL AUTO_INCREMENT,
+  `vid` int(11) DEFAULT NULL,
+  `uid` int(11) DEFAULT NULL,
+  `grade` int(11) DEFAULT NULL,
+  `detail` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`cid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `comment`
+--
+
+LOCK TABLES `comment` WRITE;
+/*!40000 ALTER TABLE `comment` DISABLE KEYS */;
+/*!40000 ALTER TABLE `comment` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `flag`
+--
+
+DROP TABLE IF EXISTS `flag`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `flag` (
+  `fid` int(11) DEFAULT NULL,
+  `vid` int(11) DEFAULT NULL,
+  `longitude` float DEFAULT NULL,
+  `latitude` float DEFAULT NULL,
+  `addtion` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `flag`
+--
+
+LOCK TABLES `flag` WRITE;
+/*!40000 ALTER TABLE `flag` DISABLE KEYS */;
+/*!40000 ALTER TABLE `flag` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `resource`
+--
+
+DROP TABLE IF EXISTS `resource`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `resource` (
+  `cid` int(11) DEFAULT NULL,
+  `url` varchar(10) DEFAULT NULL,
+  `type` int(11) DEFAULT NULL,
+  `addtion` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `resource`
+--
+
+LOCK TABLES `resource` WRITE;
+/*!40000 ALTER TABLE `resource` DISABLE KEYS */;
+/*!40000 ALTER TABLE `resource` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `user` (
+  `uid` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(20) DEFAULT NULL,
+  `password` varchar(20) DEFAULT NULL,
+  `portrait` varchar(10) DEFAULT NULL,
+  PRIMARY KEY (`uid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user`
+--
+
+LOCK TABLES `user` WRITE;
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `view`
+--
+
+DROP TABLE IF EXISTS `view`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `view` (
+  `vid` int(11) NOT NULL AUTO_INCREMENT,
+  `category` int(11) DEFAULT NULL,
+  `name` varchar(20) DEFAULT NULL,
+  `longitude` float DEFAULT NULL,
+  `latitude` float DEFAULT NULL,
+  `detail` varchar(100) DEFAULT NULL,
+  `picture` varchar(10) DEFAULT NULL,
+  `scope` varchar(300) DEFAULT NULL,
+  PRIMARY KEY (`vid`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `view`
+--
+
+LOCK TABLES `view` WRITE;
+/*!40000 ALTER TABLE `view` DISABLE KEYS */;
+INSERT INTO `view` VALUES (1,0,'复旦大学张江校区',31.1905,121.598,'复旦是个好学校',NULL,'(31.191340, 121.594715),(31.192258, 121.600959),(31.185867, 121.601515),(31.185518, 121.599015),(31.188584, 121.597717),(31.188198, 121.595410)\n');
+/*!40000 ALTER TABLE `view` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2016-07-02 15:48:20
